57523858e0610f7d3303962c371b0bc7f291448f
Nini1997 <141282707+Nini1997@users.noreply.github.com>
2023-10-04 17:52:55 -0400

    Update vehicle.js

--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,20 @@
 class Vehicle {
-    
-}
-
+    constructor(make, model, year, price, vin) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.price = price;
+      this.vin = vin;
+      this.paint = undefined; // Default paint is undefined
+    }
+  
+    paint(paintColor) {
+      this.paint = paintColor;
+    }
+  
+    drive() {
+      console.log(`This ${this.year} ${this.make} ${this.model} drives`);
+    }
+  }
+  
+  module.exports = Vehicle;
