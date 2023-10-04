06901253fa1cdfec8c7d50499e2a27e30cdd4948
Nini1997 <141282707+Nini1997@users.noreply.github.com>
2023-10-04 17:52:06 -0400

    Update factory.js

--- factory.js
+++ factory.js
@@ -1,3 +1,29 @@
-class Factory{
-    
-}
+class Factory {
+    constructor() {
+      this.vehicles = [];
+    }
+  
+    createVehicle(vehicle) {
+      if (vehicle instanceof Vehicle) {
+        this.vehicles.push(vehicle);
+      } else {
+        throw new Error("Invalid vehicle provided.");
+      }
+    }
+  
+    listVehicles() {
+      return this.vehicles;
+    }
+  
+    paintVehicle(vin, paintColor) {
+      const vehicleToPaint = this.vehicles.find((vehicle) => vehicle.vin === vin);
+  
+      if (vehicleToPaint) {
+        vehicleToPaint.paint(paintColor);
+      } else {
+        throw new Error(`Vehicle with VIN ${vin} not found.`);
+      }
+    }
+  }
+  
+  module.exports = Factory;
