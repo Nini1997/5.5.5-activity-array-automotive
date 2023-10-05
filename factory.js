742908c61f8f824039b38f0e19092433168a9d72
Nini1997 <141282707+Nini1997@users.noreply.github.com>
2023-10-04 21:26:14 -0400

    Update factory.js

--- factory.js
+++ factory.js
@@ -1,8 +1,12 @@
+// Factory class definition
 class Factory {
-    constructor() {
+    constructor(location, owner) {
+      this.location = location;
+      this.owner = owner;
       this.vehicles = [];
     }
   
+    // Method to create a new vehicle in the factory
     createVehicle(vehicle) {
       if (vehicle instanceof Vehicle) {
         this.vehicles.push(vehicle);
@@ -11,19 +15,21 @@ class Factory {
       }
     }
   
+    // Method to list all vehicles in the factory
     listVehicles() {
       return this.vehicles;
     }
   
+    // Method to paint a vehicle in the factory
     paintVehicle(vin, paintColor) {
       const vehicleToPaint = this.vehicles.find((vehicle) => vehicle.vin === vin);
-  
       if (vehicleToPaint) {
         vehicleToPaint.paint(paintColor);
       } else {
-        throw new Error(`Vehicle with VIN ${vin} not found.`);
+        throw new Error("Vehicle not found.");
       }
     }
   }
   
+  // Export the Factory class
   module.exports = Factory;
